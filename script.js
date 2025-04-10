b9fe9b338e5bafedaa149292fccec0d05fa64935
papad <papadopouloskon2001@gmail.com>
2025-04-10 19:29:17 +0300

    tickets

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
+
 const searchBar = document.querySelector('.search_bar');
 const searchIcon = document.querySelector('.search_icon');
 const searchInput = document.querySelector('.search_bar input[type="text"]');
 
-searchIcon.addEventListener('click', () => {
-  searchBar.classList.toggle('active');
-  if (searchBar.classList.contains('active')) {
-    searchInput.focus();
-  }
-});
+// searchIcon.addEventListener('click', () => {
+//   searchBar.classList.toggle('active');
+//   if (searchBar.classList.contains('active')) {
+//     searchInput.focus();
+//   }
+// });
 
-searchInput.addEventListener('blur', () => {
-  if (!searchInput.value && searchBar.classList.contains('active')) {
-    searchBar.classList.remove('active');
-  }
-});
+// searchInput.addEventListener('blur', () => {
+//   if (!searchInput.value && searchBar.classList.contains('active')) {
+//     searchBar.classList.remove('active');
+//   }
+// });
 
-
-document.getElementById('tickets_btn').addEventListener('click', function () {
-    window.location.href = 'tickets.html';
+document.querySelector('.tickets_btn').addEventListener('click', function () {
+  window.location.href = 'tickets.html';
 });
 
