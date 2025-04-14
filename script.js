e1ede90e70266b71dca4bedd15b88e785963b5c3
papad <papadopouloskon2001@gmail.com>
2025-04-15 00:23:08 +0300

    ticket types

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ document.querySelector('.tickets_btn').addEventListener('click', function () {
   window.location.href = 'tickets.html';
 });
 
+document.querySelectorAll('.ticket-counter-group').forEach(group => {
+  const input = group.querySelector('input');
+  const increment = group.querySelector('.increment');
+  const decrement = group.querySelector('.decrement');
+
+  increment.addEventListener('click', () => {
+      input.value = parseInt(input.value) + 1;
+  });
+
+  decrement.addEventListener('click', () => {
+      if (parseInt(input.value) > 0) {
+          input.value = parseInt(input.value) - 1;
+      }
+  });
+});
+
+
