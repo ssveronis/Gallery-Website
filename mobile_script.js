ec71d6eea77e8d1e4f00534852eb80b34c8a4b2b
Konstantinos Papadopoulos <papadopouloskon2001@gmail.com>
2025-04-22 19:35:41 +0300

    optimized resizing procedure

--- mobile_script.js
+++ mobile_script.js
@@ -1,8 +1,3 @@
-const searchBar = document.querySelector('.search_bar');
-const searchIcon = document.querySelector('.search_icon');
-const searchInput = document.querySelector('.search_bar input[type="text"]');
-
-
 document.querySelector('.mobile-tickets_btn').addEventListener('click', function () {
   window.location.href = 'tickets.html';
 });
