772822ed7c5b077ecfd7c2ede68d7de29c18695c
SVERONIS SOTIRIOS <62189294+ssveronis@users.noreply.github.com>
2025-05-26 11:20:19 +0300

    Account creation from terminal

--- db.js
+++ db.js
@@ -7,31 +7,51 @@ class DB {
 
     constructor() {
         //reading certificates from file
-        this.caCert = [fs.readFileSync(process.env.DB_SSL_CA_PATH, "utf8")];
-        this.clientKey = [fs.readFileSync(process.env.DB_SSL_CLIENT_KEY_PATH, "utf8")];
-        this.clientCert = [fs.readFileSync(process.env.DB_SSL_CLIENT_CRT_PATH, "utf8")];
+        try {
+            this.caCert = [fs.readFileSync(process.env.DB_SSL_CA_PATH, "utf8")];
+            this.clientKey = [fs.readFileSync(process.env.DB_SSL_CLIENT_KEY_PATH, "utf8")];
+            this.clientCert = [fs.readFileSync(process.env.DB_SSL_CLIENT_CRT_PATH, "utf8")];
+        } catch (e) {
+            this.caCert = null;
+            this.clientKey = null;
+            this.clientCert = null;
+        }
 
         this.ready = false;
         this.initDB()
     }
 
     async initDB(){
-        this.db = await mariadb.createConnection({
-            "user": process.env.DB_USER,
-            "password": process.env.DB_PASS,
-            "host": process.env.DB_HOST,
-            "port": process.env.DB_PORT,
-            "ssl": {
-                ca: this.caCert,
-                cert: this.clientCert,
-                key: this.clientKey
-            },
-            "database": process.env.DB_NAME,
-            "compress": true,
-            "connectTimeout": 500,
-            "socketTimeout": 0,
-            "queryTimeout": 200
-        })
+        if(this.caCert && this.clientKey && this.clientCert){
+            this.db = await mariadb.createConnection({
+                "user": process.env.DB_USER,
+                "password": process.env.DB_PASS,
+                "host": process.env.DB_HOST,
+                "port": process.env.DB_PORT,
+                "ssl": {
+                    ca: this.caCert,
+                    cert: this.clientCert,
+                    key: this.clientKey
+                },
+                "database": process.env.DB_NAME,
+                "compress": true,
+                "connectTimeout": 500,
+                "socketTimeout": 0,
+                "queryTimeout": 200
+            })
+        } else {
+            this.db = await mariadb.createConnection({
+                "user": process.env.DB_USER,
+                "password": process.env.DB_PASS,
+                "host": process.env.DB_HOST,
+                "port": process.env.DB_PORT,
+                "database": process.env.DB_NAME,
+                "compress": true,
+                "connectTimeout": 500,
+                "socketTimeout": 0,
+                "queryTimeout": 200
+            })
+        }
 
         this.ready = true;
 
