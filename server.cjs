c2a38034022c02bd4346bbd87bb639decab403c2
SVERONIS SOTIRIOS <62189294+ssveronis@users.noreply.github.com>
2025-05-27 15:08:27 +0300

    Small fixes

--- server.cjs
+++ server.cjs
@@ -1,9 +1,13 @@
-(async () => {
+if(process.argv[2] === "setup" && process.argv[3].includes("@")){
+    const db = new DB();
+} else {
+    (async () => {
     const { default: app } = await import('./index.js');
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
-})().catch(err => {
-    console.error('Failed to load app:', err);
-});
+    })().catch(err => {
+        console.error('Failed to load app:', err);
+    });
+}
