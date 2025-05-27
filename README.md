483c2d59522ff760c610ac9a36175eb4aeabc479
SVERONIS SOTIRIOS <62189294+ssveronis@users.noreply.github.com>
2025-05-27 14:01:41 +0300

    dist

--- README.md
+++
@@ -1,2 +0,0 @@
-![image](https://github.com/user-attachments/assets/409ddb25-c656-4487-a853-f72643bab212)
-
--- helpers.js
+++
@@ -1,9 +0,0 @@
-export function getNav(url){
-    if (url === '/admin' || url === '/admin/') return 'nav';
-    return (url.includes('admin') ? 'navAdmin' : 'nav')
-}
-
-export function isEq(par1, par2) {
-    console.log(par1, par2)
-    return par1 === par2;
-}
